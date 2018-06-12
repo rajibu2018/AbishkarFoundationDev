f31650e09eb5bbe7c042ae49a042054a35b22b58
Rajib Dutta <rajibu2010@yahoo.com>
2018-06-12 11:37:39 +0530

    commit #4 login page

---
+++ AbishKarFoundation/AbishkarFoundationWebViewModel/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AbishkarFoundation.Web.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]        
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+    }
+}
