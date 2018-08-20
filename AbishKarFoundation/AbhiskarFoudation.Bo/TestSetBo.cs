61d65d49094e197681d82eb82dca59f91c2f7c1d
Rajib Dutta <rajibu2010@yahoo.com>
2018-08-20 11:29:24 +0530

    commit #7

---
+++ AbishKarFoundation/AbhiskarFoudation.Bo/TestSetBo.cs
@@ -0,0 +1,19 @@
+﻿using AbishkarFoundation.Model;
+using System;
+
+namespace AbhiskarFoudation.Bo
+{
+    public class TestSetBo
+    {
+        public  int TestSetId { get; set; }
+        public  bool TestName { get; set; }
+        public  User Creator { get; set; }
+        public  DateTime CreatreDate { get; set; }
+        public  TestSetAccessType AccessType { get; set; }
+        public  bool RepeatedAccess { get; set; }
+        public  int? Duration { get; set; }
+        public  DateTime? ActiveUpto { get; set; }
+        public  bool Active { get; set; }
+        public int NumberOfAttender {  get; set; }
+    }
+}
---
+++ AbishKarFoundation/AbishkarFoundationRepository/Impl/TestSetRepository.cs
@@ -0,0 +1,19 @@
+﻿using AbhiskarFoudation.Bo;
+using AbishkarFoundation.Model;
+using AbishkarFoundation.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbishkarFoundation.Repository.Impl
+{
+    public class TestSetRepository : RepositoryBase, ITestSetRepository
+    {
+        public List<TestSetBo> GetTestSetByUser(int userId)
+        {
+            throw new NotImplementedException();
+            //var query= _session.QueryOver<TestSet>().Where(t=>t.Creator.UserId==userId);
+            
+        }
+    }
+}
